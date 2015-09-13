4a39ff2282c034c4a7efb99f8064b9d7fa054c0c
rballas <rballas62@gmail.com>
2015-09-13 11:38:56 -0400

    d1.cpp

---
+++ d1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+struct node {
+// data structure to hold the word
+};
+
+// Read the text from file and store it in a buffer
+bool read_text(char *filename, void *buffer, int size_of_buffer);
+
+// Get the next word from the buffer
+bool next_word(void *buffer, void *word);
+
+// Insert the word in the linked list
+void insert(void *word);
+
+// Note: You can use a helper function to insert. 
+// The main purpose of the function is to find the location in existing linked list 
+// to insert the new word.
+
+// create the list and return the alphabetically sorted output
+int main () {
+}
